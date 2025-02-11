0ed29e1ac68f9f088a701e8de6d8a4d42f5e9b59
udongmandu <qjawns10000@naver.com>
2025-02-11 16:39:45 +0900

    asdf

--- MetanetProject/src/main/java/com/classpick/web/MyappApplication.java
+++ MetanetProject/src/main/java/com/classpick/web/MyappApplication.java
@@ -2,14 +2,15 @@ package com.classpick.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.classpick.web")
 @EnableScheduling // 스케줄링 활성화
-public class MyappApplication {
+public class MyappApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyappApplication.class, args);
+    }
 
 }
